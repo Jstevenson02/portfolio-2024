8cd9db0629ab5fc3d9799569d0f8325885405d9e
jstevenson02 <jacobryanstevenson@gmail.com>
2024-04-16 14:09:29 -0400

    udate

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,32 @@
-import Image from "next/image";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
 
 export default function Home() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
   return (
     <>
       <div className='flex w-screen h-screen'>
-        <div className='w-1/2 p-4'>
-          <Link href={"/#about"}>About</Link>
+        <div className='w-1/2 p-4 flex flex-col'>
+          <Link className='navLink' href={"/#about"}>
+            ABOUT
+          </Link>
+          <Link className='navLink' href={"/#about"}>
+            EXPERIENCE
+          </Link>
+          <Link className='navLink' href={"/#about"}>
+            PROJECTS
+          </Link>
+          <div className='hover-line-text'>
+            <div className={`text-container ${isActive ? "active" : ""}`} onClick={handleClick}>
+              <span>Hover over me!</span>
+            </div>
+          </div>
         </div>
         <div className='w-1/2 p-4'>
           <div id='container' className='container'>
