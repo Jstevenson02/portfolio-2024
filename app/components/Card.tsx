12d1a1379a38d1780464863861d95d35a401ab45
jstevenson02 <jacobryanstevenson@gmail.com>
2024-04-18 10:24:37 -0400

    update

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -18,7 +18,7 @@ const Card: React.FC<ExperienceCardProps> = ({ experiences }) => {
       {experiences.map((experience, index) => (
         <li key={index} className='mb-12 max-w-lg'>
           <div className='group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover:list:opacity-50'>
-            <div className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'></div>
+            <div className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-[#191B1C]/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'></div>
             <header
               className='z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2'
               aria-label={experience.date}
@@ -29,7 +29,7 @@ const Card: React.FC<ExperienceCardProps> = ({ experiences }) => {
               <h3 className='font-medium leading-snug text-slate-200'>
                 <div>
                   <a
-                    className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base'
+                    className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-[#A800FF] focus-visible:text-[#A800FF] group/link text-base'
                     href={""}
                     target='_blank'
                     rel='noreferrer noopener'
@@ -62,7 +62,7 @@ const Card: React.FC<ExperienceCardProps> = ({ experiences }) => {
               {/* <ul className='mt-2 flex flex-wrap' aria-label='Technologies used'>
                 {technologies.map((tech) => (
                   <li key={tech} className='mr-1.5 mt-2'>
-                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-[#A800FF]'>
                       {tech}
                     </div>
                   </li>
--- app/page.tsx
+++ app/page.tsx
@@ -17,22 +17,21 @@ export default function Home() {
   };
 
   return (
-    <div className='flex justify-center h-screen scroll-smooth' onWheel={handleWheel}>
-      <div className='flex flex-col'>
-        <Hero />
-        <Navbar />
-      </div>
-
+    <div className='lg:flex lg:justify-between lg:gap-4'>
       <div
-        className='flex flex-col overflow-y-auto scroll-smooth pt-24 lg:w-1/2 lg:py-24'
-        ref={rightPanelRef}
+        className='lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24'
+        onWheel={handleWheel}
       >
-        <div className='container mx-auto`'>
-          <p id='about' className=''>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Pretium nibh ipsum consequat nisl vel
-            pretium lectus quam.
-          </p>
+        <div>
+          <Hero />
+          <Navbar />
+        </div>
+      </div>
+      <div ref={rightPanelRef}>
+        <div id='about'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Pretium nibh ipsum consequat nisl vel pretium lectus
+          quam.
         </div>
         <div id='experience'>
           <Card experiences={experiences} />
@@ -42,3 +41,27 @@ export default function Home() {
     </div>
   );
 }
+
+// <div className='flex h-screen scroll-smooth' onWheel={handleWheel}>
+//   <div className='flex flex-col'>
+//     <Hero />
+//     <Navbar />
+//   </div>
+
+//   <div
+//     className='flex flex-col overflow-y-auto scroll-smooth pt-24  lg:py-24'
+//     ref={rightPanelRef}
+//   >
+//     <div className='container mx-auto`'>
+//       <p id='about' className='max-w-md p-10'>
+//         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+//         incididunt ut labore et dolore magna aliqua. Pretium nibh ipsum consequat nisl vel
+//         pretium lectus quam.
+//       </p>
+//     </div>
+//     <div id='experience'>
+//       <Card experiences={experiences} />
+//     </div>
+//     <div id='projects'>test</div>
+//   </div>
+// </div>;
