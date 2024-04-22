9fa7eab6a6f389e1c79aafac044b3efa6e34c30f
jstevenson02 <jacobryanstevenson@gmail.com>
2024-04-22 14:41:47 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -21,9 +21,9 @@ export default function Home() {
   };
 
   return (
-    <div className='lg:flex min-h-screen'>
+    <div className='lg:flex items-center justify-center min-h-screen'>
       {/* Left Container */}
-      <div className='lg:fixed top-0 bottom-0 lg:w-1/2 p-10 space-y-6' onWheel={handleWheel}>
+      <div className='lg:fixed top-0 bottom-0 lg:w-1/3 p-10 space-y-6' onWheel={handleWheel}>
         <Hero />
         <nav>
           <Navbar />
@@ -31,76 +31,35 @@ export default function Home() {
       </div>
 
       {/* Right panel */}
-      <div
-        className='ml-auto max-w-screen-md lg:w-1/2 lg:overflow-auto flex flex-col justify-center pr-10'
-        ref={rightPanelRef}
-      >
-        <div className='mobileNavLink'>
-          <h2 className='mobileNavText'>About</h2>
+      <div>
+        <div
+          className='ml-auto max-w-screen-xl lg:w-1/2 lg:overflow-auto flex flex-col justify-center pr-10'
+          ref={rightPanelRef}
+        >
+          <div className='mobileNavLink'>
+            <h2 className='mobileNavText'>About</h2>
+          </div>
+          <div className='p-6'>
+            <About />
+          </div>
+          <div className='mobileNavLink'>
+            <h2 className='mobileNavText'>
+              <Link href='/#experience'>Experience</Link>
+            </h2>
+          </div>
+          <div id='experience' className='p-6'>
+            <ExperienceCard />
+          </div>
+          <div className='mobileNavLink'>
+            <h2 className='mobileNavText'>
+              <Link href='/#projects'>Projects</Link>
+            </h2>
+          </div>
+          <div id='projects' className='p-6'>
+            <ProjectCard />
+          </div>
         </div>
-        <div className='p-6'>
-          <About />
-        </div>
-        <div className='mobileNavLink'>
-          <h2 className='mobileNavText'>
-            <Link href='/#experience'>Experience</Link>
-          </h2>
-        </div>
-        <div id='experience' className='p-6'>
-          <ExperienceCard />
-        </div>
-        <div className='mobileNavLink'>
-          <h2 className='mobileNavText'>
-            <Link href='/#projects'>Projects</Link>
-          </h2>
-        </div>
-        <div id='projects' className='p-6'>
-          <ProjectCard />
-        </div>
-      </div>
-      <div className='lg:bottom-0 lg:fixed p-6 flex justify-center items-center'>
-        <Links />
       </div>
     </div>
-    // <div className='lg:flex lg:justify-between min-h-screen'>
-    //   <div
-    //     className='lg:sticky lg:top-0 lg:max-h-screen lg:w-1/2 lg:flex-col lg:py-2'
-    //     onWheel={handleWheel}
-    //   >
-    //     <div className='flex flex-col'>
-    //       <div className='w-full p-4 mt-4'>
-    //         <Hero />
-    //       </div>
-    //       <div className='w-full p-4 mt-4'>
-    //         <Navbar />
-    //       </div>
-    //     </div>
-    //     <div className='lg:bottom-0 lg:fixed'>
-    //       <Links />
-    //     </div>
-    //   </div>
-    //   <div className='lg:w-1/2 lg:py-24' ref={rightPanelRef}>
-    //     <div className='mobileNavLink'>
-    //       <h2 className='mobileNavText'>About</h2>
-    //     </div>
-    //     <About />
-    //     <div className='mobileNavLink'>
-    //       <h2 className='mobileNavText'>
-    //         <Link href='/#experience'>Experience</Link>
-    //       </h2>
-    //     </div>
-    //     <div id='experience'>
-    //       <ExperienceCard experiences={experiences} />
-    //     </div>
-    //     <div className='mobileNavLink'>
-    //       <h2 className='mobileNavText'>
-    //         <Link href='/#projects'>Projects</Link>
-    //       </h2>
-    //     </div>
-    //     <div className='mt-32' id='projects'>
-    //       <ProjectCard />
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
