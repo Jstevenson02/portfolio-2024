7bcb79f9a6b1330576d0a69abcfa002c7890bfb5
jstevenson02 <jacobryanstevenson@gmail.com>
2024-04-25 12:27:06 -0400

    update

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -36,24 +36,25 @@ const ProjectCardData: React.FC<ProjectCardDataProps> = ({
               aria-label={`${live_code_link} (opens in a new tab)`}
             >
               <span className='absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block'></span>
-              <span>{name}</span>
+              <span>
+                {name}
 
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 20 20'
-                fill='currentColor'
-                className='inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px'
-              >
-                <path
-                  fillRule='evenodd'
-                  d='M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z'
-                  clipRule='evenodd'
-                ></path>
-              </svg>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 20 20'
+                  fill='currentColor'
+                  className='inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px'
+                >
+                  <path
+                    fillRule='evenodd'
+                    d='M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z'
+                    clipRule='evenodd'
+                  ></path>
+                </svg>
+              </span>
             </a>
           </h3>
-          <p className='mt-2 text-[13px] leading-normal text-[#9B9B9B]'>{description}</p>
-
+          <p className='mt-2 text-sm leading-normal text-[#9B9B9B]'>{description}</p>
           <ul className='mt-2 flex flex-wrap' aria-label='Technologies used:'>
             {tags.map((tech, index) => (
               <li key={index} className='mr-1.5 mt-2'>
@@ -64,11 +65,12 @@ const ProjectCardData: React.FC<ProjectCardDataProps> = ({
             ))}
           </ul>
         </div>
+
         <Image
           alt={`${name} marketing card`}
           loading='lazy'
           width='200'
-          height='48'
+          height='0'
           decoding='async'
           data-nimg='1'
           className='rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1'
