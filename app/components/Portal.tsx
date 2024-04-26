9af7ca86688264ef8ae386ff0d11054ca0132734
jstevenson02 <jacobryanstevenson@gmail.com>
2024-04-26 12:22:47 -0400

    update

--- app/components/Portal.tsx
+++ app/components/Portal.tsx
@@ -6,13 +6,12 @@ import Modal from "./Modal";
 const Portal = () => {
   const [showModal, setShowModal] = useState(false);
   return (
-    <button
-      onClick={() => setShowModal(true)}
-      className='hover:-text-teal-300 absolute bottom-0 right-0 inline-flex items-center px-2 py-4 font-medium text-slate-400 hover:-translate-y-2 focus-visible:text-teal-300'
-    >
-      <Image className='w-20' alt='portal to older sites' src={portal} />
-      <Modal showModal={showModal} setShowModal={setShowModal} />
-    </button>
+    <div className='relative'>
+      <button onClick={() => setShowModal(true)} className='z-50 right-0 bottom-0 absolute'>
+        <Image className='w-20' alt='portal to older sites' src={portal} />
+        <Modal showModal={showModal} setShowModal={setShowModal} />
+      </button>
+    </div>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -125,8 +125,8 @@ export default function Home() {
 
           <Footer />
         </main>
-        <Portal />
       </div>
+      <Portal />
     </div>
   );
 }
