a89d15b2092059fa1ea92d336da9c4b5211cd4cb
jstevenson02 <jacobryanstevenson@gmail.com>
2024-05-09 20:45:16 -0400

    u

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
         Jacob Stevenson
       </a>
       <h2 className='mt-3 text-base font-medium tracking-tight text-slate-200  lg:text-lg '>
-        Junior Frontend Engineer
+        Software Engineer
       </h2>
       <p className='mt-4 max-w-sm text-[#9b9b9b] leading-normal'>
         I develop web applications, user interfaces, and backend databases.
