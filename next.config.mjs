931f1be2ce6f8475c227074ca283402d0b774bd9
jstevenson02 <jacobryanstevenson@gmail.com>
2024-12-31 10:34:20 -0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@react-pdf/renderer"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/links",
+        permanent: true, // Use `true` for 301 (permanent) redirects or `false` for 302 (temporary) redirects
+      },
+    ];
+  },
 };
 
 export default nextConfig;
